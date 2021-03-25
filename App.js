cfd32740c040c0b6319cab9766283b496b14633e
Mladen Raykov <45385472+MadJiGit@users.noreply.github.com>
2021-03-25 18:32:31 +0200

    first

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react'; 
+import Main from './app/components/Main';
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
